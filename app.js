39b3ffcfeac9acbe14a9b8308f11c3a3690df235
sultan singh <sultansingh731@gmail.com>
2022-06-05 19:47:33 +0530

    pushpa :fire:

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ app.use(
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-app.post("/convert-mp3", async (req, res) => {
-  const videoId = req.body.videoID;
+app.post("/", async (req, res) => {
+  const videoId = extrectVideoId(req.body.videoID)
+  // const videoId = req.body.videoID;
   console.log(videoId);
-  if (videoId === "undefined" || videoId === "" || videoId === null) {
+  if (videoId === undefined || videoId === "" || videoId === null) {
     return res.render("index", {
       success: false,
       message: " please enter the valid video ID",
@@ -44,8 +45,8 @@ app.post("/convert-mp3", async (req, res) => {
 
     axios
       .request(options)
-      .then(response => response.data)
-      .then( (data) => {
+      .then((response) => response.data)
+      .then((data) => {
         console.log(data);
         if (data.status === "ok") {
           return res.render("index", {
@@ -53,10 +54,36 @@ app.post("/convert-mp3", async (req, res) => {
             song_title: data.title,
             song_link: data.link,
           });
-        } else return res.render("index", { success: false, message: data.msg });
+        } else
+          return res.render("index", { success: false, message: data.msg });
       })
       .catch(function (error) {
         console.error(error);
       });
   }
 });
+
+// "https://youtu.be/B5_6jfGsyjA"
+// https://www.youtube.com/watch?v=B5_6jfGsyjA
+// B5_6jfGsyjA
+
+
+function extrectVideoId(videoUrl) {
+  try {
+    if (videoUrl.includes("/")) {
+      if (videoUrl.includes("?v=")) {
+        const arr = videoUrl.split("?v=");
+        return arr[1];
+      } else {
+        const arr = videoUrl.split("/");
+        return arr[arr.length - 1];
+      }
+    }
+    return videoUrl;
+    
+  } catch (error) {
+    return undefined
+  }
+  
+
+}
